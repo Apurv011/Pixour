b750247668d56e8bd14e7cf3dfe5502ecad5ce40
Aputv011 <apurvdak@gmail.com>
2021-06-12 23:42:32 +0530

    added site

---
+++ pixour-app/src/components/Examples.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Examples(){
+
+  return (
+    <div id="ex" className="examples">
+      <div className="container">
+        <div className="common">
+          <h1 className="mainHeader">Examples</h1>
+
+          <div className="commonBorder"></div>
+
+        </div>
+        <div className="row">
+        <div className="center22">
+        <img src="images/stycon.jpg" alt="content_and_style_img"/>
+        </div>
+        <div className="mainContent common3" style={{color:"#ffffff",fontSize:"20px"}}>
+          Content And Style Image
+        </div>
+        </div>
+        <br/>
+        <br/>
+        <div className="row">
+        <div className="mainContent common3" style={{color:"#ffffff",fontSize:"20px"}}>
+          Output Image
+        </div>
+        <div className="center22">
+        <img src="images/fin.jpg" alt="output_img"/>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Examples;
---
+++ pixour-app/src/components/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Home(){
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div className="header__content">
+              <div className="header__section">
+
+                <h1 style={{color:"#1F2235"}}>Pixour</h1>
+                <p style={{color:"#1F2235"}}>A deep learning model based on neural style transfer</p>
+                <div className="header__buttons">
+                  <a href="https://colab.research.google.com/drive/1Wi56fuiVUs8VQ6Z8sW18RP9fyhGmwKe7?usp=sharing" className="btn btn-outline" style={{color:"#1F2235"}}>
+                    Open With Colab
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="banner__img">
+              <img src="images/bg1.jpg" style={{height:"600px",width:"700px",marginTop:"100px"}}className="col-12" alt="man" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
